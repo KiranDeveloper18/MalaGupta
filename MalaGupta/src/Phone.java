2b1bd13a3dd7dac3e688992dd562956a761e719a
kiran orsu <kiran orsu@192.168.0.7>
2021-02-15 20:57:23 +0530

    about class.

---
+++ MalaGupta/src/Phone.java
@@ -0,0 +1,34 @@
+
+public class Phone 
+{
+    String model;
+    String company;
+    
+   
+    Phone (String model)     //constructor 
+    {
+    	this.model= model;
+    }
+    
+    double weight;
+	 
+    
+    void makeCall(String numbers)
+    {
+    	 //code     
+    }
+    void recieveCall()
+    {
+       // code
+    }
+	public static void main(String[] args) 
+	{
+       Phone p1 = new Phone("iphone");
+       
+       
+	}
+	
+
+	
+	
+}
